79245f8c951ab8869de66bc0de5b5d96838f4e9a
Megha Parayarukattil Rajan <Mrajan8764@conestogac.on.ca>
2023-06-04 18:13:16 -0400

    Initial Version

---
+++ ClassLibrary1/TriangleSolver_Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class TriangleSolver_Test
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
