89ba54192f24dc245d1e82186aca7e9901bde7c9
Megha Parayarukattil Rajan <Mrajan8764@conestogac.on.ca>
2023-06-04 18:55:46 -0400

    Equilateral Triangle Test

--- ClassLibrary1/TriangleSolver_Test.cs
+++ ClassLibrary1/TriangleSolver_Test.cs
@@ -5,16 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriangleSolver;
 
 namespace UnitTest
 {
     [TestFixture]
     public class TriangleSolver_Test
     {
-        [Test]
-        public void Test()
-        {
+       
 
+        [SetUp]
+        public void Setup()
+        {
+            
         }
+
+        [Test]
+        public void test_AnalyzeTriangle_Equilateral()
+        {
+            // Arrange
+            int sideA = 5;
+            int sideB = 5;
+            int sideC = 5;
+            string expectedType = "The triangle is valid and is an EQUILATERAL";
+
+            // Act
+            string actualType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual(expectedType, actualType);
+        }
+        
     }
 }
