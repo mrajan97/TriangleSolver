8181b22a675b554a5ef1b7b00a3094d2b5031e46
Megha Parayarukattil Rajan <Mrajan8764@conestogac.on.ca>
2023-06-07 22:02:50 -0400

    Commit

---
+++ TriangleSolver_TestProject/TriangleSolverUnitTest.cs
@@ -0,0 +1,11 @@
+namespace TriangleSolver_TestProject
+{
+    [TestClass]
+    public class TriangleSolverUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TriangleSolver_UnitTest/TriangleSolverTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleSolver_UnitTest
+{
+    [TestFixture]
+    public class TriangleSolverTest
+    {
+    }
+}
